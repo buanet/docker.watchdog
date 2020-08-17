127cd35ea3ded8cea959c535a2360bb1c72297d9
buanet <info@buanet.de>
2020-08-17 23:39:01 +0200

    back to alpine

--- amd64/scripts/healthcheck.sh
+++ amd64/scripts/healthcheck.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Docker engine API v1.24 (https://docs.docker.com/engine/api/v1.24/)
 curl --max-time 10 --no-buffer -s --unix-socket /var/run/docker.sock --fail http://localhost/_ping || exit 1
